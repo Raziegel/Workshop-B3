8337c31b60dff522a0fc15eeaf448c35e9ab7b3f
Egel <yoann.mazza1@gmail.com>
2017-10-02 16:49:54 +0200

    fix list

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { HomePage } from '../home/home';
-import { ListPage } from '../list/list';
-import { ConnectionPage } from '../connection/connection';
 @Component({
+  selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
   // this tells the list component which Pages
   // should be each tab's root Page
-  tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
 
   constructor(public navCtrl: NavController) {
-    if(!localStorage.getItem("token")) {
-      navCtrl.setRoot(LoginPage);
-    }
+
   }
 }
