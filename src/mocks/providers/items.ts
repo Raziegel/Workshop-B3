a1a2d82fd7f83c3624800e93e0f21924b9c5c8e2
Egel <yoann.mazza1@gmail.com>
2017-10-03 13:54:06 +0200

    liste items work

--- src/mocks/providers/items.ts
+++ src/mocks/providers/items.ts
@@ -18,37 +18,108 @@ export class Items {
       {
         "name": "Burt Bear",
         "profilePic": "assets/img/speakers/bear.jpg",
-        "about": "Burt is a Bear."
+        "about": "Burt is a Bear.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "open",
+        "entrepriseCliente" :"LatêtedanslaToile"
+
       },
       {
         "name": "Charlie Cheetah",
         "profilePic": "assets/img/speakers/cheetah.jpg",
-        "about": "Charlie is a Cheetah."
+        "about": "Charlie is a Cheetah.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "win",
+        "entrepriseCliente" :"LatêtedanslaToile"
       },
       {
         "name": "Donald Duck",
         "profilePic": "assets/img/speakers/duck.jpg",
-        "about": "Donald is a Duck."
+        "about": "Donald is a Duck.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "win",
+        "entrepriseCliente" :"LatêtedanslaToile"
       },
       {
         "name": "Eva Eagle",
         "profilePic": "assets/img/speakers/eagle.jpg",
-        "about": "Eva is an Eagle."
+        "about": "Eva is an Eagle.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "lost",
+        "entrepriseCliente" :"LatêtedanslaToile"
       },
       {
         "name": "Ellie Elephant",
         "profilePic": "assets/img/speakers/elephant.jpg",
-        "about": "Ellie is an Elephant."
+        "about": "Ellie is an Elephant.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "win",
+        "entrepriseCliente" :"LatêtedanslaToile"
       },
       {
         "name": "Molly Mouse",
         "profilePic": "assets/img/speakers/mouse.jpg",
-        "about": "Molly is a Mouse."
+        "about": "Molly is a Mouse.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "lost",
+        "entrepriseCliente" :"LatêtedanslaToile"
       },
       {
         "name": "Paul Puppy",
         "profilePic": "assets/img/speakers/puppy.jpg",
-        "about": "Paul is a Puppy."
+        "about": "Paul is a Puppy.",
+        "dateDemande" : "01/01",
+        "titre" : "TITRE!",
+        "description" : "lorem",
+        "lieu" : "Paris",
+        "prix" : "120$",
+        "duree" : "20 mois",
+        "frequence" : "2 jours/semaines",
+        "dateDeDepartPrevuAuPlusTard" : "01/06/2017",
+        "statut" : "open",
+        "entrepriseCliente" :"LatêtedanslaToile"
       }
     ];
 
